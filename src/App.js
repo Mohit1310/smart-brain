cfa4e3048cf01b2c45e5e97b56a58088f18de0e4
dev_only <dev_only@Bs-MacBook-Pro.local>
2023-03-08 11:31:29 -0600

    fixing errors

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from "react";
-import ParticlesBg from "particles-bg";
-import Clarifai from "clarifai";
-import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
-import Navigation from "./components/Navigation/Navigation";
-import SignIn from "./components/SignIn/SignIn";
-import Register from "./components/Register/Register";
-import Logo from "./components/Logo/Logo";
-import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
-import Rank from "./components/Rank/Rank";
-import "./App.css";
+import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Navigation from './components/Navigation/Navigation';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
 
 const app = new Clarifai.App({
-  apiKey: "78614678cf8b43988ffe0786cba7fc9a",
+  apiKey: '78614678cf8b43988ffe0786cba7fc9a',
 });
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: "",
-      imageUrl: "",
+      input: '',
+      imageUrl: '',
       box: {},
-      route: "signin",
+      route: 'signin',
       isSignedIn: false,
     };
   }
@@ -29,13 +29,13 @@ class App extends Component {
   calculateFaceLocation = (data) => {
     const clarifaiFace =
       data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById("inputImage");
+    const image = document.getElementById('inputImage');
     const width = Number(image.width);
     const height = Number(image.height);
     console.log(width, height);
     return {
       leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row.height,
+      topRow: clarifaiFace.top_row * height,
       rightCol: width - clarifaiFace.right_col * width,
       bottomRow: height - clarifaiFace.bottom_row * height,
     };
@@ -43,37 +43,41 @@ class App extends Component {
 
   displayFaceBox = (box) => {
     this.setState({ box: box });
+    
   };
 
   onInputChange = (event) => {
+    console.log(event.target.value);
     this.setState({ imageUrl: event.target.value });
   };
 
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
+    // this.setState({ input: this.state.imageUrl });
+    // console.log(this.state.imageUrl);
     app.models
       .predict(
         {
-          id: "face-detection",
-          name: "face-detection",
-          version: "6dc7e46bc9124c5c8824be4822abe105",
-          type: "visual-detector",
-          // id: "a403429f2ddf4b49b307e318f00e528b",
-          // version: "34ce21a40cc24b6b96ffee54aabff139",
+          // id: "face-detection",
+          // name: "face-detection",
+          // version: "6dc7e46bc9124c5c8824be4822abe105",
+          // type: "visual-detector",
+          id: 'a403429f2ddf4b49b307e318f00e528b',
+          version: '34ce21a40cc24b6b96ffee54aabff139',
         },
-        this.state.input
+        // this.state.input
+        this.state.imageUrl
       )
       .then((response) => {
-        console.log("hi", response);
+        console.log('hi', response);
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
-    .catch((err) => console.log("error", err));
+      .catch((err) => console.log('error', err));
   };
 
   onRouteChange = (route) => {
-    if (route === "signout") {
+    if (route === 'signout') {
       this.setState({ isSignedIn: false });
-    } else if (route === "home") {
+    } else if (route === 'home') {
       this.setState({ isSignedIn: true });
     }
     this.setState({ route: route });
@@ -89,7 +93,7 @@ class App extends Component {
           isSignedIn={isSignedIn}
           onRouteChange={this.onRouteChange}
         />
-        {route === "home" ? (
+        {route === 'home' ? (
           <div>
             <Logo />
             <Rank />
@@ -99,7 +103,7 @@ class App extends Component {
             />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
-        ) : route === "signin" ? (
+        ) : route === 'signin' ? (
           <SignIn onRouteChange={this.onRouteChange} />
         ) : (
           <Register onRouteChange={this.onRouteChange} />
